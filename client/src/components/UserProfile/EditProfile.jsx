b4991be976d1a57bc42dfc8dd262307cc672a475
Harsh Sharma <harshsharma90866@gmail.com>
2024-08-13 05:59:10 +0530

    Add redirect button

--- client/src/components/UserProfile/EditProfile.jsx
+++ client/src/components/UserProfile/EditProfile.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { Navigate, useParams, useNavigate } from "react-router-dom";
 import axios from "axios";
 
 
@@ -11,6 +11,8 @@ function EditProfile() {
   const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
   const token = localStorage.getItem("token");
   const { userId } = useParams();
+  const navigate   = useNavigate() ; 
+
 
     const handleSubmit = async(e) => { 
       
@@ -26,37 +28,42 @@ function EditProfile() {
 
         })
         setEmail("") ; 
-        setUsername("") ; 
+        setUsername("") ;
+        navigate(`/login/user-profile/${userId}`)
       } catch (error) {
         console.error("Error posting user data:", error.message);
       }
     }
   return (
     <>
-    <div>EditProfile</div>
-    <form className="flex flex-col" onSubmit={handleSubmit}>
-      <label>
-        Provide the username to change.
-      </label>
-      <input 
-        type ="text"
-        value = {username}
-        name = "username"
-        onChange={(e) => setUsername(e.target.value)} />
+   
 
-<label>
-        Provide the email to change.
-      </label>
-      <input 
-        type ="email"
-        value = {email}
-        name = "email-address"
-        onChange={(e) => setEmail(e.target.value)} />
-        
-        <button type ="submit">
-          Submit
-        </button>
-    </form>
+
+<form className="max-w-sm mx-auto" onSubmit={handleSubmit} >
+  <div className="mb-5">
+    <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+    <input
+    type ="email"
+    value = {email}
+    name = "email-address"
+    onChange={(e) => setEmail(e.target.value)}
+      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@gmail.com"  />
+  </div>
+  <div className="mb-5">
+    <label htmlFor="username" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"> Change your  username</label>
+    <input 
+    type ="text"
+    value = {username}
+    name = "username"
+    onChange={(e) => setUsername(e.target.value)} 
+     className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+     placeholder="abcdef"
+      />
+  </div>
+  
+  <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+</form>
+
     </>
 
     
--- client/src/components/UserProfile/UserProfile.jsx
+++ client/src/components/UserProfile/UserProfile.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams  ,Link} from "react-router-dom";
 import axios from "axios";
 import { FaUserCircle } from "react-icons/fa";
 
@@ -76,9 +76,15 @@ function UserProfile() {
     <button type="button" className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">
         Create a fund me project.
     </button>
-    <button type="button" className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">
-        Edit profile
+    <Link to={`/login/edit-profile/${userId}`}>
+    <button
+      type="button"
+      className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+    >
+      Edit profile
     </button>
+  </Link>
+    
     <button type="button" className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">
         Change password.
     </button>
