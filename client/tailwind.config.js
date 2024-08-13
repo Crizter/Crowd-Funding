3fc400de4114bfb3ea01b26a02b6e731c259b423
Harsh Sharma <harshsharma90866@gmail.com>
2024-08-13 23:35:07 +0530

    Add DatePicker React component

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,10 +1,10 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+import withMT from "@material-tailwind/react/utils/withMT";
+export default withMT({
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
     'node_modules/flowbite-react/lib/esm/**/*.js',
-
   ],
   theme: {
     extend: {},
@@ -12,4 +12,4 @@ export default {
   plugins: [
     require('flowbite/plugin')
   ],
-}
+});
