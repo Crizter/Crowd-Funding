c3d3e5d3a3a626be790b9010420310f536782fd9
Harsh Sharma <harshsharma90866@gmail.com>
2024-08-14 06:49:30 +0530

    Add about us in route

--- client/src/components/RoutesComponent.jsx
+++ client/src/components/RoutesComponent.jsx
@@ -11,7 +11,7 @@ import SinglePage from './Pages/SinglePage.jsx';
 import UserProfile from './UserProfile/UserProfile.jsx';
 import EditProfile from './UserProfile/EditProfile.jsx';
 import Category from './Projects/Category.jsx';
-
+import AboutUs from './Pages/AboutUs.jsx';
 
 
 
@@ -23,7 +23,10 @@ function RoutesComponent() {
           <Routes>
         {/* Authentication Routes */}
         <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />       
+        <Route path="/login" element={<Login />} />  
+             
+        
+
         {/* * Protected Routes */}  
         <Route path="/login/dashboard/user-dashboard/:userId/*" element={<ProtectedRoutes element={Dashboard} />} /> 
         <Route path= "/login/home/:userId/*" element = {<ProtectedRoutes element={ Home}/>} />
@@ -31,6 +34,7 @@ function RoutesComponent() {
         <Route path = "/login/user-profile/:userId/*" element = {<ProtectedRoutes element={UserProfile} /> } />
         <Route path = "/login/edit-profile/:userId/*" element = {<ProtectedRoutes element={EditProfile} /> } />
         <Route path = "/login/category-project/:userId/*" element = {<ProtectedRoutes element={Category}/>} />
+        <Route path = "/about-us/:userId" element = {<ProtectedRoutes element = {AboutUs} />}/>
       </Routes>
   </Layout>
     </Router>
