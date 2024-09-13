b8270d12b619bda586205f3c67274413f24caf41
Harsh Sharma <harshsharma90866@gmail.com>
2024-09-13 18:19:32 +0530

    cors

--- server/server.js
+++ server/server.js
@@ -16,15 +16,15 @@ const port = process.env.BACKEND_PORT || 3003 ;
 const secretOrKey  = process.env.SECRETKEY
         
 // MIDDLEWARES 
-// const corsOptions = {
-//   origin : 'http://localhost:5173',
-//   credentials : true, 
-//   methods: ['GET', 'POST', 'PUT', 'DELETE'], 
-//   allowedHeaders: ['Content-Type', 'Authorization'],
-// }
-//   app.use(cors(corsOptions)) ;
+const corsOptions = {
+  origin : 'https://crowd-funding-client-7n0o2n1u0-crizters-projects.vercel.app',
+  credentials : true, 
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}
+  app.use(cors(corsOptions)) ;
 
-app.use(cors())
+// app.use(cors())
 app.use(urlencoded({ extended: true }));
 app.use(express.json());
 app.use(passport.initialize()) ; 
